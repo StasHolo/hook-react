71e904cbf3f6f76106542998039c7f2127b96616
Stas <Stass1994@yandex.ru>
2023-09-12 20:26:25 +0500

    g

--- src/myComponents/TaskList.js
+++ src/myComponents/TaskList.js
@@ -5,7 +5,7 @@ import { useReducer } from "react";
 
 export const TaskList = () => {
 const [newTaskText, setNewTaskText] = useState('');
-const [TaskChange, setTaskChange] = useState(' |Не выполнена');
+const [TaskStatus, setTaskStatus] = useState(' |Не выполнена');
 const initialState = [];
 const [tasks, dispatch] = useReducer(taskReducer, initialState);
 
@@ -22,13 +22,17 @@ function taskReducer(state, action) {
 
 function addTask(text) {
     dispatch({ type: 'ADD_TASK', text });
+    setTaskStatus([...TaskStatus, false]);
   }
   
   function deleteTask(id) {
     dispatch({ type: 'DELETE_TASK', id });
+    //setTaskStatus((prevStatus) => prevStatus.filter((status, index) => index !== id));
   }
-function acceptTask() { 
-    setTaskChange(' |Выполнена');
+function acceptTask(index) { 
+    setTaskStatus((prevStatus) =>
+      prevStatus.map((status, i) => (i === index ? !status : status))
+      );
 }
 
 
@@ -36,14 +40,16 @@ function acceptTask() {
     return (
         <div>
             <ul>
-                {tasks.map(task => (
-                    <li key={task.id}>
-                        {task.text}
-                        {TaskChange}
-                        <button onClick={() => deleteTask(task.id)}>Удалить</button>
-                        <button onClick={acceptTask}>Выполнить</button>
-                    </li>
-                ))}
+            {tasks.map((task, index) => (
+          <li key={task.id}>
+            {task.text}
+            {TaskStatus[index] ? " | Не выполнено" : " | Выполнено"}
+            <button onClick={() => deleteTask(task.id)}>Удалить</button>
+            <button onClick={() => acceptTask(index)}>
+              {TaskStatus[index] ? "Выполнить" : "Отменить"}
+            </button>
+          </li>
+        ))}
             </ul>
             <input
                 type="text"
